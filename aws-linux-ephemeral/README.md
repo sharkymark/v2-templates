86b70cfe0e8a33f6c7be4dd20f461fceeadb8771
Mark Milligan <mtm20176@gmail.com>
2023-05-20 08:37:27 -0500

    feat: aws ec2 ephemeral vm with persistent /home/coder volume

--- aws-linux-ephemeral/README.md
+++ aws-linux-ephemeral/README.md
@@ -1,6 +1,6 @@
 ---
 name: Develop in Linux on AWS EC2 (Ephemeral, Persistent Home)
-description: Get started with Linux development on AWS EC2.
+description: Get started with Linux development on AWS EC2 where only the home volume is persisted.
 tags: [cloud, aws]
 icon: /icon/aws.png
 ---
@@ -9,74 +9,21 @@ icon: /icon/aws.png
 
 This template gives developers ephemeral AWS instances. When a workspace is restarted, all data is lost except files in `/home/coder/` which is persisted by a seperate EBS volume.
 
-To add software (e.g. Java), you have a few options:
-
-1. Template authors can install software in the [startup_script](https://github.com/bpmct/coder-templates/blob/b804db6da6e2702058d38148f1b9d44a23e83c1e/aws-linux-ephemeral/main.tf#L82)
-1. Template authors can build [custom AMIs](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/creating-an-ami-ebs.html) which include all packages. New template versions can reference the new AMI, even in [build pipelines](http://coder.com/docs/v2/latest/templates/change-management).
-1. Developers can add an executable `personalize` script in their home directory to install unique software.
-
 ## Authentication
 
-This template assumes that coderd is run in an environment that is authenticated
-with AWS. For example, run `aws configure import` to import credentials on the
-system and user running coderd. For other ways to authenticate [consult the
-Terraform docs](https://registry.terraform.io/providers/hashicorp/aws/latest/docs#authentication-and-configuration).
+This template prompts the template administrator for the AWS credentials which are stored in the Coder database
 
-## Required permissions / policy
+## Web IDE
 
-The following sample policy allows Coder to create EC2 instances and modify
-instances provisioned by Coder:
-
-```json
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "VisualEditor0",
-      "Effect": "Allow",
-      "Action": [
-        "ec2:GetDefaultCreditSpecification",
-        "ec2:DescribeIamInstanceProfileAssociations",
-        "ec2:DescribeTags",
-        "ec2:CreateTags",
-        "ec2:RunInstances",
-        "ec2:DescribeInstanceCreditSpecifications",
-        "ec2:DescribeImages",
-        "ec2:ModifyDefaultCreditSpecification",
-        "ec2:DescribeVolumes"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Sid": "CoderResources",
-      "Effect": "Allow",
-      "Action": [
-        "ec2:DescribeInstances",
-        "ec2:DescribeInstanceAttribute",
-        "ec2:UnmonitorInstances",
-        "ec2:TerminateInstances",
-        "ec2:StartInstances",
-        "ec2:StopInstances",
-        "ec2:DeleteTags",
-        "ec2:MonitorInstances",
-        "ec2:CreateTags",
-        "ec2:RunInstances",
-        "ec2:ModifyInstanceAttribute",
-        "ec2:ModifyInstanceCreditSpecification"
-      ],
-      "Resource": "arn:aws:ec2:*:*:instance/*",
-      "Condition": {
-        "StringEquals": {
-          "aws:ResourceTag/Coder_Provisioned": "true"
-        }
-      }
-    }
-  ]
-}
-```
-
-## code-server
-
-`code-server` is installed via the `startup_script` argument in the `coder_agent`
+`code-server`, Coder's VS Code in a browser OSS project, is installed via the `startup_script` argument in the `coder_agent`
 resource block. The `coder_app` resource is defined to access `code-server` through
 the dashboard UI over `localhost:13337`.
+
+## Dotfiles
+
+At workspace creation, the user can enter their dotfiles repository which Coder clones and runs in the `startup_script`
+
+## Resources
+
+[EBS Volumes and NVMe on Linux instances](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/nvme-ebs-volumes.html)
+
--- aws-linux-ephemeral/main.tf
+++ aws-linux-ephemeral/main.tf
@@ -2,58 +2,179 @@ terraform {
   required_providers {
     coder = {
       source  = "coder/coder"
-      version = "0.6.6"
     }
   }
 }
 
-# Last updated 2022-05-31
-# aws ec2 describe-regions | jq -r '[.Regions[].RegionName] | sort'
-variable "region" {
-  description = "What region should your workspace live in?"
-  default     = "us-east-1"
-  validation {
-    condition = contains([
-      "ap-northeast-1",
-      "ap-northeast-2",
-      "ap-northeast-3",
-      "ap-south-1",
-      "ap-southeast-1",
-      "ap-southeast-2",
-      "ca-central-1",
-      "eu-central-1",
-      "eu-north-1",
-      "eu-west-1",
-      "eu-west-2",
-      "eu-west-3",
-      "sa-east-1",
-      "us-east-1",
-      "us-east-2",
-      "us-west-1",
-      "us-west-2"
-    ], var.region)
-    error_message = "Invalid region!"
-  }
+provider "coder" {
+  feature_use_managed_variables = "true"
 }
 
-variable "instance_type" {
-  description = "What instance type should your workspace use?"
-  default     = "t3.micro"
-  validation {
-    condition = contains([
-      "t3.micro",
-      "t3.small",
-      "t3.medium",
-      "t3.large",
-      "t3.xlarge",
-      "t3.2xlarge",
-    ], var.instance_type)
-    error_message = "Invalid instance type!"
-  }
+variable "access-key-id" {
+  sensitive   = true
+  description = <<-EOF
+  Enter an AWS Access key id
+
+  EOF
+  default = ""
+}
+
+variable "secret-access-key" {
+  sensitive   = true
+  description = <<-EOF
+  Enter an AWS Secret Access key
+
+  EOF
+  default = ""
 }
 
 provider "aws" {
-  region = var.region
+  region      = data.coder_parameter.region.value
+  access_key  = var.access-key-id
+  secret_key  = var.secret-access-key 
+}
+
+# To retrieve latest regions from AWS CLI
+# aws ec2 describe-regions | jq -r '[.Regions[].RegionName] | sort'
+data "coder_parameter" "region" {
+  name        = "Region"
+  description = "The region to deploy the workspace in."
+  default     = "us-west-1"
+  mutable     = false
+
+  option {
+    name  = "US West (N. California)"
+    value = "us-west-1"
+    icon  = "/emojis/1f1fa-1f1f8.png"
+  }
+  option {
+    name  = "US East (N. Virginia)"
+    value = "us-east-1"
+    icon  = "/emojis/1f1fa-1f1f8.png"
+  }  
+  option {
+    name  = "US West (Oregon)"
+    value = "us-west-2"
+    icon  = "/emojis/1f1fa-1f1f8.png"
+  }
+  option {
+    name  = "South America (São Paulo)"
+    value = "sa-east-1"
+    icon  = "/emojis/1f1e7-1f1f7.png"
+  }
+  option {
+    name  = "Asia Pacific (Tokyo)"
+    value = "ap-northeast-1"
+    icon  = "/emojis/1f1ef-1f1f5.png"
+  }
+  option {
+    name  = "Asia Pacific (Seoul)"
+    value = "ap-northeast-2"
+    icon  = "/emojis/1f1f0-1f1f7.png"
+  }
+  option {
+    name  = "Asia Pacific (Osaka-Local)"
+    value = "ap-northeast-3"
+    icon  = "/emojis/1f1f0-1f1f7.png"
+  }
+  option {
+    name  = "Asia Pacific (Mumbai)"
+    value = "ap-south-1"
+    icon  = "/emojis/1f1f0-1f1f7.png"
+  }
+  option {
+    name  = "Asia Pacific (Singapore)"
+    value = "ap-southeast-1"
+    icon  = "/emojis/1f1f0-1f1f7.png"
+  }
+  option {
+    name  = "Asia Pacific (Sydney)"
+    value = "ap-southeast-2"
+    icon  = "/emojis/1f1f0-1f1f7.png"
+  }
+  option {
+    name  = "Canada (Central)"
+    value = "ca-central-1"
+    icon  = "/emojis/1f1e8-1f1e6.png"
+  }
+  option {
+    name  = "EU (Frankfurt)"
+    value = "eu-central-1"
+    icon  = "/emojis/1f1ea-1f1fa.png"
+  }
+  option {
+    name  = "EU (Stockholm)"
+    value = "eu-north-1"
+    icon  = "/emojis/1f1ea-1f1fa.png"
+  }
+  option {
+    name  = "EU (Ireland)"
+    value = "eu-west-1"
+    icon  = "/emojis/1f1ea-1f1fa.png"
+  }
+  option {
+    name  = "EU (London)"
+    value = "eu-west-2"
+    icon  = "/emojis/1f1ea-1f1fa.png"
+  }
+  option {
+    name  = "EU (Paris)"
+    value = "eu-west-3"
+    icon  = "/emojis/1f1ea-1f1fa.png"
+  }   
+}
+
+data "coder_parameter" "instance_type" {
+  name        = "EC2 Instance Type"
+  description = "What instance type should your workspace use?"
+  default     = "t3.micro"
+  mutable     = false
+  option {
+    name  = "2 vCPU, 1 GiB RAM t3.micro"
+    value = "t3.micro"
+  }
+  option {
+    name  = "2 vCPU, 2 GiB RAM t3.small"
+    value = "t3.small"
+  } 
+  option {
+    name  = "2 vCPU, 4 GiB RAM t3.medium"
+    value = "t3.medium"
+  }
+  option {
+    name  = "2 vCPU, 8 GiB RAM t3.large"
+    value = "t3.large"
+  }
+  option {
+    name  = "4 vCPU, 16 GiB RAM t3.xlarge"
+    value = "t3.xlarge"
+  }
+  option {
+    name  = "8 vCPU, 32 GiB RAM t3.2xlarge"
+    value = "t3.2xlarge"
+  }
+}
+
+data "coder_parameter" "disk_size" {
+  name        = "Home volume persistent storage size"
+  type        = "number"
+  description = "Number of GB of storage"
+  icon        = "https://www.pngall.com/wp-content/uploads/5/Database-Storage-PNG-Clipart.png"
+  validation {
+    min       = 15
+    max       = 50
+  }
+  mutable     = false
+  default     = 15
+}
+
+data "coder_parameter" "dotfiles_url" {
+  name        = "Dotfiles URL"
+  description = "Personalize your workspace"
+  type        = "string"
+  default     = "git@github.com:sharkymark/dotfiles.git"
+  mutable     = true 
+  icon        = "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
 }
 
 data "coder_workspace" "me" {
@@ -75,27 +196,67 @@ data "aws_ami" "ubuntu" {
 resource "coder_agent" "main" {
   arch           = "amd64"
   auth           = "aws-instance-identity"
+
+
+  metadata {
+    display_name = "Memory Usage"
+    key  = "pod-mem"
+    script = "awk '{ print $1 }' /sys/fs/cgroup/memory/memory.usage_in_bytes | numfmt --to=iec"
+    interval = 1
+    timeout = 1
+  }
+
+  metadata {
+    display_name = "CPU Usage"
+    key  = "cpu"
+    # calculates CPU usage by summing the "us", "sy" and "id" columns of
+    # vmstat.
+    script = <<EOT
+        top -bn1 | awk 'FNR==3 {printf "%2.0f%%", $2+$3+$4}'
+        #vmstat | awk 'FNR==3 {printf "%2.0f%%", $13+$14+$16}'
+    EOT
+    interval = 10
+    timeout = 1
+  }
+
+  metadata {
+    display_name = "Disk Usage"
+    key  = "disk"
+    script = "df /home/coder | awk NR==2'{print $5}'"
+    interval = 600
+    timeout = 1
+  }
+
+  metadata {
+    display_name = "Memory Used"
+    key  = "mem3"
+    script = <<EOT
+    free | awk '/^Mem/ { printf("%.0f%%", $3/$2 * 100.0) }'
+    EOT
+    interval = 10
+    timeout = 1
+  }   
+
   os             = "linux"
   startup_script = <<EOT
     #!/bin/bash
 
     # install and start code-server
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.8.3 | tee code-server-install.log
-    code-server --auth none --port 13337 | tee code-server-install.log &
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server
+    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
 
-    # Run personalize script, if possibleß
-    if [ -x ~/personalize ]; then
-      ~/personalize | tee -a ~/.personalize.log
-    elif [ -f ~/personalize ]; then
-      echo "~/personalize is not executable, skipping..." | tee -a ~/.personalize.log
+    # use coder CLI to clone and install dotfiles
+    if [[ ! -z "${data.coder_parameter.dotfiles_url.value}" ]]; then
+      coder dotfiles -y ${data.coder_parameter.dotfiles_url.value}
     fi
+
   EOT
 }
 
 resource "coder_app" "code-server" {
   agent_id     = coder_agent.main.id
   slug         = "code-server"
-  display_name = "code-server"
+  display_name = "VS Code Web"
   url          = "http://localhost:13337/?folder=/home/coder"
   icon         = "/icon/code.svg"
   subdomain    = false
@@ -168,8 +329,8 @@ EOT
 resource "aws_instance" "dev" {
   count             = data.coder_workspace.me.start_count
   ami               = data.aws_ami.ubuntu.id
-  availability_zone = "${var.region}a"
-  instance_type     = var.instance_type
+  availability_zone = "${data.coder_parameter.region.value}a"
+  instance_type     = data.coder_parameter.instance_type.value
 
   user_data = local.user_data
   tags = {
@@ -181,8 +342,8 @@ resource "aws_instance" "dev" {
 
 # Persistent EBS "home" volume (for /home/coder)
 resource "aws_ebs_volume" "home" {
-  availability_zone = "${var.region}a"
-  size              = 15
+  availability_zone = "${data.coder_parameter.region.value}a"
+  size              = data.coder_parameter.disk_size.value
 
   tags = {
     Name = "coder-homedir-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
@@ -203,10 +364,18 @@ resource "coder_metadata" "workspace_info" {
   resource_id = aws_instance.dev[0].id
   item {
     key   = "region"
-    value = var.region
+    value = data.coder_parameter.region.value
   }
   item {
     key   = "instance type"
     value = aws_instance.dev[0].instance_type
   }
+  item {
+    key   = "availability zone"
+    value = aws_ebs_volume.home.availability_zone
+  }  
+  item {
+    key   = "home volume size"
+    value = "${data.coder_parameter.disk_size.value}GB"
+  }    
 }
