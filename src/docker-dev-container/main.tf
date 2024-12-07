1607bc6edd27cfef5e45d1c1e299c1268b4bdc5c
Mark Milligan <mtm20176@gmail.com>
2024-12-07 16:01:56 -0600

    chore: app helpers docker dev container

--- src/docker-dev-container/main.tf
+++ src/docker-dev-container/main.tf
@@ -139,7 +139,7 @@ resource "coder_agent" "main" {
     vscode = true
     vscode_insiders = false
     ssh_helper = false
-    port_forwarding_helper = true
+    port_forwarding_helper = false
     web_terminal = true
   }
 
